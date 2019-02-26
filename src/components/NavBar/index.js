6c456ee83eea1181e1dc53c2147b7263f847f3fe
P  J <jaegersen@gmail.com>
2019-02-26 03:41:42 +0000

    edited the top nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -15,7 +15,7 @@ const NavBar = ({toggleNavbar, isActive}) => (
       <nav className='navbar is-fixed-top' aria-label='main navigation'>
         <div className='navbar-brand'>
           <Link to='/' className='navbar-item'>
-            <strong>Gatsby Starter Business</strong>
+            <strong>Straights and Curves</strong>
           </Link>
           <button
             className={`button navbar-burger ${isActive ? 'is-active' : ''}`}
@@ -30,15 +30,9 @@ const NavBar = ({toggleNavbar, isActive}) => (
         <div className={`navbar-menu ${isActive ? 'is-active' : ''}`} id='navMenu'>
 
           <div className='navbar-end'>
-            <SearchBox searchIndex={data.siteSearchIndex.index} />
+            
             <Link className='navbar-item' to='/about'>
                             About
-            </Link>
-            <Link className='navbar-item' to='/pricing'>
-                            Pricing
-            </Link>
-            <Link className='navbar-item' to='/blog'>
-                            Blog
             </Link>
             <div className='navbar-item'>
               <div className='field is-grouped'>
